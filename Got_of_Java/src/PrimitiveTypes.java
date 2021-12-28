479bbbe6edd4cb6a583c2346b2833fc42fbd0dce
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2021-12-28 10:43:31 +0900

    Initial Commit

--- Got_of_Java/src/PrimitiveTypes.java
+++ Got_of_Java/src/PrimitiveTypes.java
@@ -1,2 +1,23 @@
 public class PrimitiveTypes {
+    public void checkByte(){
+        byte byteMin = -128;
+        byte byteMax = 127;
+        System.out.println("byteMin = "+byteMin);
+        System.out.println("byteMax = "+byteMax);
+        byteMin = (byte)(-128 -1);
+        byteMax = (byte)(127+1);
+        System.out.println("byteMin-1 = "+byteMin);
+        System.out.println("byteMax+1 = "+byteMax);
+    }
+
+    public void checkOtherTypes(){
+        short shortMax = 32767;
+        int intMax = 2147483647;
+        long longMax = 9223372036854775807L;
+    }
+
+    public static void main(String[] args) {
+        PrimitiveTypes types = new PrimitiveTypes();
+        types.checkByte();
+    }
 }
