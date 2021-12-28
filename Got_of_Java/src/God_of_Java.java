6df953b81fdc076ca74b9a25c1633bc62337b936
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2021-12-28 10:01:00 +0900

    Initial Commit

---
+++ Got_of_Java/src/God_of_Java.java
@@ -0,0 +1,12 @@
+public class God_of_Java {
+    public static int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(add(1,2));
+        System.out.println("my name is hyeonjeong");
+
+    }
+}
