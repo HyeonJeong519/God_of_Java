2bba0dc8798e75a5197c4b9fecf3d9b7c7952dfe
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2022-01-04 09:24:33 +0900

    Initial Commit

---
+++ Got_of_Java/src/OperatorComparision.java
@@ -0,0 +1,10 @@
+public class OperatorComparision {
+    public static void main(String[] args) {
+        OperatorComparision operator = new OperatorComparision();
+        operator.compareboolean();
+    }
+    public void compareboolean(){
+        boolean a = true;
+        System.out.println(true==a);
+    }
+}
